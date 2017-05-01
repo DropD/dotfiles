802f5fe885a46260fbcdeea55dd10ae6d432fc94
DropD <r.haeuselmann@gmx.ch>
2017-05-01 21:50:29 +0200

    fix commentor

--- nvim/local/ch.ricoh.commentor/ftplugin/commentor.py
+++ nvim/local/ch.ricoh.commentor/ftplugin/commentor.py
@@ -6,16 +6,19 @@ __py_version = version_info.major == 3 and '3' or ''
 
 
 class Commentor(object):
+    """Handles the actual commenting"""
     def __init__(self, cstr):
         self._cstr = cstr + '~ '
 
     def all_comment(self, s, st, en):
+        """(bool) is the whole range commented out?"""
         for i in range(st, en):
             if self._cstr not in s[i]:
                 return False
         return True
 
     def toggle_comment(self):
+        """toggles the comment string at the start of the range"""
         r = vim.current.range
         rlen = r.end - r.start + 1
         ac = self.all_comment(r, 0, rlen)
@@ -27,20 +30,25 @@ class Commentor(object):
 
 
 class CommentorRegistry(object):
+    """Holds refs to all the needed commentors"""
     def __init__(self, name):
-        from sys import version_info
         self.__registry = {}
         self.__py_version = version_info.major == 3 and '3' or ''
         self.__name = name
 
-    def set_commentor(self, lang, *args, commentor_cls=Commentor):
+    def set_commentor(self, lang, *args, **kwargs):
+        """create and set the commentor for a specific language"""
+        commentor_cls = kwargs.get('commentor_cls', Commentor)
         self.__registry[lang] = commentor_cls(*args)
 
-    def add_commentor(self, lang, *args, commentor_cls=Commentor):
+    def add_commentor(self, lang, *args, **kwargs):
+        """check if a commentor exists and call set_commentor if necessary"""
+        commentor_cls = kwargs.get('commentor_cls', Commentor)
         if lang not in self.__registry:
             self.set_commentor(lang, *args, commentor_cls=commentor_cls)
 
     def activate_commentor(self, lang):
+        """activate a specific language commentor"""
         if lang in self.__registry:
             cmd = ("{mode}map <Leader>' :py{version} "
                    "{reg_name}['{lang}'].toggle_comment() <CR>")
@@ -50,14 +58,17 @@ class CommentorRegistry(object):
                                    reg_name=self.__name, lang=lang))
 
     def set_lang(self, lang, cstr):
+        """make commentor ready for the current buffer"""
         self.add_commentor(lang, cstr)
         self.activate_commentor(lang)
 
     @property
     def registry(self):
+        """the dictionary of all current commentors"""
         return self.__registry
 
     def __getitem__(self, lang):
+        """dict style access to commentors"""
         return self.__registry[lang]
 
 
