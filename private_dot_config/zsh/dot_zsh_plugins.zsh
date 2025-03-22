3daf916a849eed0ab7007f73c59921e2b1aa6c96
Rico Häuselmann <rico.haeuselmann@gmail.com>
2025-03-22 22:01:35 +0100

    add zsh and omp configs from ephebe

---
+++ private_dot_config/zsh/dot_zsh_plugins.zsh
@@ -0,0 +1,29 @@
+fpath+=( /Users/ricoh/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions )
+source /Users/ricoh/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions/zsh-completions.plugin.zsh
+fpath+=( /Users/ricoh/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/git )
+source /Users/ricoh/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/git/git.plugin.zsh
+fpath+=( /Users/ricoh/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/pip )
+source /Users/ricoh/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/pip/pip.plugin.zsh
+fpath+=( /Users/ricoh/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/brew )
+source /Users/ricoh/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/brew/brew.plugin.zsh
+fpath+=( /Users/ricoh/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/command-not-found )
+source /Users/ricoh/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/command-not-found/command-not-found.plugin.zsh
+fpath+=( /Users/ricoh/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zpm-zsh-SLASH-zshmarks )
+source /Users/ricoh/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zpm-zsh-SLASH-zshmarks/bookmarks.plugin.zsh
+fpath+=( /Users/ricoh/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-alexrochas-SLASH-zsh-git-semantic-commits )
+source /Users/ricoh/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-alexrochas-SLASH-zsh-git-semantic-commits/git-semantic-commits.plugin.zsh
+fpath+=( /Users/ricoh/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-alexrochas-SLASH-zsh-path-environment-explorer )
+source /Users/ricoh/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-alexrochas-SLASH-zsh-path-environment-explorer/zsh-path-environment-explorer.plugin.zsh
+fpath+=( /Users/ricoh/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting )
+source /Users/ricoh/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
+fpath+=( /Users/ricoh/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-apple-touchbar )
+source /Users/ricoh/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-apple-touchbar/zsh-apple-touchbar.plugin.zsh
+fpath+=( /Users/ricoh/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-esc-SLASH-conda-zsh-completion )
+source /Users/ricoh/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-esc-SLASH-conda-zsh-completion/conda-zsh-completion.plugin.zsh
+fpath+=( /Users/ricoh/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-laggardkernel-SLASH-zsh-iterm2 )
+source /Users/ricoh/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-laggardkernel-SLASH-zsh-iterm2/init.zsh
+source /Users/ricoh/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-laggardkernel-SLASH-zsh-iterm2/iterm2_shell_integration.zsh
+source /Users/ricoh/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-laggardkernel-SLASH-zsh-iterm2/zsh-iterm2.zsh
+fpath+=( /Users/ricoh/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-powerlevel10k )
+source /Users/ricoh/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-powerlevel10k/powerlevel10k.zsh-theme
+source /Users/ricoh/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-powerlevel10k/powerlevel9k.zsh-theme
